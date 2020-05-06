af39df71490f0f0db6f207f1999bf62cbdb98b88
Nick Fox-Gieg <nick@fox-gieg.com>
2020-05-06 13:11:39 -0400

    restore 2018 version of gdrive plugin

---
+++ ThirdParty/UnityGoogleDrive/Examples/Scripts/TestFilesEmptyTrash.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityGoogleDrive;
+
+public class TestFilesEmptyTrash : AdaptiveWindowGUI
+{
+    private GoogleDriveFiles.EmptyTrashRequest request;
+    private string result;
+
+    protected override void OnWindowGUI (int windowId)
+    {
+        if (request != null && request.IsRunning)
+        {
+            GUILayout.Label(string.Format("Loading: {0:P2}", request.Progress));
+        }
+        else
+        {
+            if (GUILayout.Button("Empty Trash")) EmptyTrash();
+        }
+        if (!string.IsNullOrEmpty(result))
+        {
+            GUILayout.Label(result);
+        }
+    }
+
+    private void EmptyTrash ()
+    {
+        request = GoogleDriveFiles.EmptyTrash();
+        request.Send().OnDone += _ => result = request.IsError ? request.Error : "trash emptied";
+    }
+}
